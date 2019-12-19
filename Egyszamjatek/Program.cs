20be0b982ce70cd0e300d2e26259d98e06be5db9
Tóth Lajos <lajos.toth@windowslive.com>
2019-12-19 12:40:45 +0100

    Egyszamjatek feladat

--- Egyszamjatek/Program.cs
+++ Egyszamjatek/Program.cs
@@ -3,13 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Egyszamjatek
 {
     class Program
     {
+        class Jatekos
+        {
+            public string Nev;
+            public List<int> Tipplista = new List<int>();           
+        }
+
+
         static void Main(string[] args)
         {
+            const string file = "egyszamjatek.txt";
+            StreamReader sr = new StreamReader(file);
+            List<Jatekos> Jatekosok = new List<Jatekos>();
+
+            string sor;
+            string[] adatsor;            
+
+            while (!sr.EndOfStream)
+            {
+                Jatekos temp = new Jatekos();
+                sor = sr.ReadLine();
+                adatsor = sor.Split(' ');
+                temp.Nev = adatsor[0];
+
+                for (int i = 1; i < adatsor.Length; i++)
+                {
+                    temp.Tipplista.Add(int.Parse(adatsor[i]));
+                }
+
+                Jatekosok.Add(temp);
+            }
+            sr.Close();
+
+
+
         }
     }
 }
